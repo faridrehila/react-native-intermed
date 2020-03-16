758d80d79a1bf5261cc6208156322ebb60d5199b
Farid Rehila <farid.rehila@gmail.com>
2020-03-16 13:48:26 +0100

    Removing useless functions

--- src/components/FeedsEpic/FeedsEpicNavigator.js
+++ src/components/FeedsEpic/FeedsEpicNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation';
-import {navigateOnce} from '../../util/navigationUtil';
 
 import FeedsScreen from './FeedsScreen/FeedsScreen';
 import SourceScreen from './SourceScreen/SourceScreen';
