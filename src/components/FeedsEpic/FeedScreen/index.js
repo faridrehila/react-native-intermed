b6dbaa7d21a20bba84d10aab2cccff52648a1412
Farid Rehila <farid.rehila@gmail.com>
2020-03-16 14:37:24 +0100

    Dark in feed screen -> changing articles to dark mode w/ webview

--- src/components/FeedsEpic/FeedScreen/index.js
+++ src/components/FeedsEpic/FeedScreen/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
 import PropTypes from 'prop-types';
+import {ThemeContext, DARK_THEME} from '../../../context/ThemeProvider';
 
 export default function FeedScreen({
   navigation: {
@@ -10,9 +11,33 @@ export default function FeedScreen({
     },
   },
 }) {
+  const {currentTheme} = useContext(ThemeContext);
+
+  const jsCode =
+    currentTheme === DARK_THEME
+      ? `
+    setTimeout(() => {      
+      let body = document.getElementsByTagName('body');
+      for (let e of body) { e.style.color = "white"; e.style.backgroundColor = "black"; }            
+      document.body.style.backgroundColor = "black";
+
+      let divs = document.getElementsByTagName('div');      
+      for (let e of divs) { e.style.color = "white"; e.style.backgroundColor = "black"; }            
+
+      let ps = document.getElementsByTagName('p');      
+      for (let e of ps) { e.style.color = "white"; e.style.backgroundColor = "black"; }    
+      
+      let spans = document.getElementsByTagName('span');      
+      for (let e of spans) { e.style.color = "white"; e.style.backgroundColor = "black"; }    
+
+      document.f1.submit(); 
+    }, 100);
+`
+      : ``;
+
   return (
     <View style={styles.container}>
-      <WebView source={{uri: feedUrl}} />
+      <WebView source={{uri: feedUrl}} injectedJavaScript={jsCode} />
     </View>
   );
 }
